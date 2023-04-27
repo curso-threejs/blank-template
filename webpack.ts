3ccf7d81af4c2ccb8b7a0fbc937901b5fad65b88
Juan Lambre <juanmlambre@gmail.com>
2023-04-27 14:46:05 -0300

    Add public folder

--- webpack.ts
+++ webpack.ts
@@ -16,6 +16,10 @@ module.exports = {
 
   devServer: {
     open: true,
+    static: {
+      directory: path.resolve(__dirname, "public"),
+      publicPath: "/public",
+    },
   },
 
   plugins: [
